f6b7eca1e86e6ac0c0c1e456120c092e00f7edc0
emilymetzgar93 <emilymetzgar93@gmail.com>
2021-03-25 11:07:47 -0500

    added changes

--- README.md
+++ README.md
@@ -1,90 +1,44 @@
 # 06 Server-Side APIs: Weather Dashboard
 
-## Your Task
+## Description
+A simple third-party APIs application that allows users to access a weather dashboard that will run in the browser and feature dynamically updated HTML and CSS.
 
-Third-party APIs allow developers to access their data and functionality by making requests with specific parameters to a URL. Developers are often tasked with retrieving data from another application's API and using it in the context of their own. Your challenge is to build a weather dashboard that will run in the browser and feature dynamically updated HTML and CSS.
+## Installation
 
-Use the [OpenWeather API](https://openweathermap.org/api) to retrieve weather data for cities. The documentation includes a section called "How to start" that provides basic setup and usage instructions. You will use `localStorage` to store any persistent data.
+Runs in the browser and features HTML, CSS, bootstrap, and is powered by Javascript that uses jQuery as well. 
 
-## User Story
+## Usage
 
-```
-AS A traveler
-I WANT to see the weather outlook for multiple cities
-SO THAT I can plan a trip accordingly
-```
+Organizes weather data to allow users to search for a particular city and view weather to plan trips accordingly. 
 
-## Acceptance Criteria
+![A screenshot of the application](./assets/images/screenshot.gif)
 
-```
-GIVEN a weather dashboard with form inputs
-WHEN I search for a city
-THEN I am presented with current and future conditions for that city and that city is added to the search history
-WHEN I view current weather conditions for that city
-THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-WHEN I view the UV index
-THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-WHEN I view future weather conditions for that city
-THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
-WHEN I click on a city in the search history
-THEN I am again presented with current and future conditions for that city
-```
+link to application: https://emilymetzgar.github.io/05-work-day-scheduler/
 
-## Mock-Up
+## Credits
 
-The following image shows the web application's appearance and functionality:
+Emily Metzgar
 
-![The weather app includes a search option, a list of cities, and a five-day forecast and current weather conditions for Atlanta.](./Assets/06-server-side-apis-homework-demo.png)
+## License
 
-## Grading Requirements
+MIT License
 
-This homework is graded based on the following criteria: 
+Copyright (c) [2021] [Emily Metzgar SMU Coding Bootcamp]
 
-### Technical Acceptance Criteria: 40%
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-* Satisfies all of the above acceptance criteria plus the following:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
 
-    * Uses the OpenWeather API to retrieve weather data.
-
-    * Uses `localStorage` to store persistent data.
-
-### Deployment: 32%
-
-* Application deployed at live URL.
-
-* Application loads with no errors.
-
-* Application GitHub URL submitted.
-
-* GitHub repository that contains application code.
-
-### Application Quality: 15%
-
-* Application user experience is intuitive and easy to navigate.
-
-* Application user interface style is clean and polished.
-
-* Application resembles the mock-up functionality provided in the homework instructions.
-
-### Repository Quality: 13%
-
-* Repository has a unique name.
-
-* Repository follows best practices for file structure and naming conventions.
-
-* Repository follows best practices for class/id naming conventions, indentation, quality comments, etc.
-
-* Repository contains multiple descriptive commit messages.
-
-* Repository contains quality readme file with description, screenshot, and link to deployed application.
-
-## Review
-
-You are required to submit BOTH of the following for review:
-
-* The URL of the functional, deployed application.
-
-* The URL of the GitHub repository. Give the repository a unique name and include a readme describing the project.
-
-- - -
-© 2021 Trilogy Education Services, LLC, a 2U, Inc. brand. Confidential and Proprietary. All Rights Reserved.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,147 @@
-//d52bfbcfacc4ca103f3cb67d31cb1af6
 
-console.log("hello world");
+$(document).ready(function () {
+    //store variables for weather data to display on html and apis
+    var historyContainer = $('#old-searches');
+    var formSearch = $('#search');
+    var todayContainer = $('#today');
+    var forecastContainer = $('#forecast');
+    var apiKey = 'd52bfbcfacc4ca103f3cb67d31cb1af6';
+    var baseUrl = 'https://api.openweathermap.org/data/2.5/weather?';
+    var baseUrl2 = 'https://api.openweathermap.org/data/2.5/forecast?';
+    var iconUrl = 'http://openweathermap.org/img/w/';
+    var baseUviURL = 'https://api.openweathermap.org/data/2.5/onecall?';
+    var historySearches = [];
+    // when a user searches for a city, run this function
+    formSearch.submit(function (event) {
+        event.preventDefault();
+
+        // this = this form that was submitted
+        var values = $(this).serializeArray();
+        var cities = values[0].value;
+        //create element w/jQuery selector
+        var searchDiv = $('<button type = "button" class= "btn search-div">');
+        searchDiv.click(function(event) {
+            event.preventDefault();
+        });
+        historySearches.push(cities);//push user city search with actual cities from api
+        localStorage.setItem('historySearches', JSON.stringify(historySearches));
+        searchDiv.text(cities);
+        historyContainer.append(searchDiv);
+
+
+        //actual data retreived from user in form
+        searchWeather(cities);
+        searchForecast(cities);
+
+    });
+    //function to search weather data based on city
+    //fetches url 
+    function searchWeather(cities) {
+        var wholeUrl = baseUrl + "q=" + cities + "&appid=" + apiKey;
+
+        fetch(wholeUrl)
+        //then JSON
+
+            .then(function (response) {
+                return response.json();
+            })//and then fetch data with specific data
+            .then(function (data) {
+
+                var wind = data.wind;
+                var humid = data.main.humidity;
+                var citiesNames = data.name;
+                var temperatures = data.main.temp;
+                var weather = data.weather;
+                var iconUrlArr = iconUrl + weather[0].icon + '.png';
+                var namesDiv = $("<div class = 'display-cities-names'>");//show on html
+                var humidDiv = $("<div class = 'display-humid'>");//show on html
+                var tempsDiv = $("<div class = 'display-temps'>");//show on html
+                var windyDiv = $("<div class = 'display-windy'>");//show on html
+                var weatherImg = $("<img class = 'icon-name' />");//show on html
+                weatherImg.attr('src', iconUrlArr);//show on html
+                namesDiv.text(citiesNames);//show on html
+                tempsDiv.text("Temperature: " + temperatures);//show on html
+                humidDiv.text("Humidity: " + humid + "%");//show on html
+                windyDiv.text("Wind Speeds: " + wind.speed + "MPH");//show on html
+                todayContainer.append(weatherImg);//append to today container on html
+                todayContainer.append(namesDiv);//append to today container on html
+                todayContainer.append(tempsDiv);//append to today container on html
+                todayContainer.append(humidDiv);//append to today container on html
+                todayContainer.append(windyDiv);//append to today container on html
+
+            });
+    }
+    //search future weather, 5 day forecast
+    function searchForecast(cities) {
+        var forecastBaseUrl2 = baseUrl2 + "q=" + cities + "&appid=" + apiKey;
+        fetch(forecastBaseUrl2).then(function (rawResponse) {
+            return rawResponse.json()
+        }).then(function (data) {
+            console.log('Forecast', data);
+            var coordinates = data.city.coord;
+            getUVI(coordinates.lat, coordinates.lon);
+            //array that collects data for 5 day forecast
+
+            for (var i = 0; i < data.list.length; i++) {
+                //update at specific time and use moment for date
+                var timeThreeOClock = data.list[i].dt_txt.search('15:00:00');
+                var cityName = data.city.name;
+                if (timeThreeOClock > -1) {
+                    var forecastTime = data.list[i];
+                    var temp = forecastTime.main.temp;
+                    var wind = forecastTime.wind;
+                    var humid = forecastTime.main.humidity;
+                    var day = moment(forecastTime.dt_txt).format('dddd, MMMM Do');
+                    var weather = forecastTime.weather;
+                    var iconUrlArr = iconUrl + weather[0].icon + '.png';
+
+                    var rows = $("<div class='col-2'>")
+                    var dayDiv = $("<div class = 'display-day'>");
+                    var humidDiv = $("<div class = 'display-humid'>");
+                    var tempsDiv = $("<div class = 'display-temps'>");
+                    var windyDiv = $("<div class = 'display-windy'>");
+                    var weatherImg = $("<img class = 'icon-name' />")
+                    weatherImg.attr('src', iconUrlArr)
+                    dayDiv.text(day);
+                    tempsDiv.text("Temperature: " + temp);
+                    humidDiv.text("Humidity: " + humid + "%");
+                    windyDiv.text("Wind Speeds: " + wind.speed + "MPH");
+                    rows.append(weatherImg);
+                    rows.append(dayDiv);
+                    rows.append(humidDiv);
+                    rows.append(tempsDiv);
+                    rows.append(windyDiv);
+                    forecastContainer.append(rows);
+                }
+            }
+        });
+    }
+
+    function getUVI(lat, lon) {
+    //lat=33.441792&lon=-94.037689&exclude=hourly,daily&appid={API key}
+        var UviURL = baseUviURL + 'lat=' + lat + '&lon=' + lon + '&exclude=hourly,daily&appid=' + apiKey;
+        fetch(UviURL).then(function(response) {
+            return response.json();
+        }).then(function(data) {
+            console.log('UV DATA', data)
+            var uviInfo = data.current.uvi;
+            todayContainer.append("UV Index: " + uviInfo);
+        });
+    }//store in local storage, get from local storage, display on html
+    function getHistorySearches() {
+        if (localStorage.getItem('historySearches')) {
+            historySearches = JSON.parse(localStorage.getItem('historySearches'));
+            for (var i = 0; i < historySearches.length; i++) {
+                var searchDiv = $('<button type = "button" class= "btn search-div">');
+                searchDiv.click(function(event) {
+                    event.preventDefault();
+                });
+                searchDiv.text(historySearches[i]);
+                historyContainer.append(searchDiv);
+                
+            }
+        }
+    }
+  
+    getHistorySearches();
+});
