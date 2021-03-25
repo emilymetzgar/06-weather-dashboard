d114cc8f448e20912d7a65a799df724c1cb9a1ef
emilymetzgar93 <emilymetzgar93@gmail.com>
2021-03-25 12:43:09 -0500

    removed extra search div

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function () {
     //store variables for weather data to display on html and apis
     var historyContainer = $('#old-searches');
@@ -12,7 +11,7 @@ $(document).ready(function () {
     var baseUviURL = 'https://api.openweathermap.org/data/2.5/onecall?';
     var historySearches = [];
     // when a user searches for a city, run this function
-    formSearch.submit(function (event) {
+        formSearch.submit(function (event) {
         event.preventDefault();
 
         // this = this form that was submitted
@@ -20,14 +19,13 @@ $(document).ready(function () {
         var cities = values[0].value;
         //create element w/jQuery selector
         var searchDiv = $('<button type = "button" class= "btn search-div">');
-        searchDiv.click(function(event) {
+        searchDiv.click(function (event) {
             event.preventDefault();
         });
-        historySearches.push(cities);//push user city search with actual cities from api
+        historySearches.push(cities); //push user city search with actual cities from api
         localStorage.setItem('historySearches', JSON.stringify(historySearches));
         searchDiv.text(cities);
-        historyContainer.append(searchDiv);
-
+        //historyContainer.append(searchDiv);
 
         //actual data retreived from user in form to run search functions 
         //for todays weather and 5 day forecast
@@ -41,11 +39,11 @@ $(document).ready(function () {
         var wholeUrl = baseUrl + "q=" + cities + "&appid=" + apiKey;
 
         fetch(wholeUrl)
-        //then JSON
+            //then JSON
 
             .then(function (response) {
                 return response.json();
-            })//and then fetch data with specific data
+            }) //and then fetch data with specific data
             .then(function (data) {
 
                 var wind = data.wind;
@@ -54,21 +52,21 @@ $(document).ready(function () {
                 var temperatures = data.main.temp;
                 var weather = data.weather;
                 var iconUrlArr = iconUrl + weather[0].icon + '.png';
-                var namesDiv = $("<div class = 'display-cities-names'>");//show on html
-                var humidDiv = $("<div class = 'display-humid'>");//show on html
-                var tempsDiv = $("<div class = 'display-temps'>");//show on html
-                var windyDiv = $("<div class = 'display-windy'>");//show on html
-                var weatherImg = $("<img class = 'icon-name' />");//show on html
-                weatherImg.attr('src', iconUrlArr);//show on html
-                namesDiv.text(citiesNames);//show on html
-                tempsDiv.text("Temperature: " + temperatures);//show on html
-                humidDiv.text("Humidity: " + humid + "%");//show on html
-                windyDiv.text("Wind Speeds: " + wind.speed + "MPH");//show on html
-                todayContainer.append(weatherImg);//append to today container on html
-                todayContainer.append(namesDiv);//append to today container on html
-                todayContainer.append(tempsDiv);//append to today container on html
-                todayContainer.append(humidDiv);//append to today container on html
-                todayContainer.append(windyDiv);//append to today container on html
+                var namesDiv = $("<div class = 'display-cities-names'>"); //show on html
+                var humidDiv = $("<div class = 'display-humid'>"); //show on html
+                var tempsDiv = $("<div class = 'display-temps'>"); //show on html
+                var windyDiv = $("<div class = 'display-windy'>"); //show on html
+                var weatherImg = $("<img class = 'icon-name' />"); //show on html
+                weatherImg.attr('src', iconUrlArr); //show on html
+                namesDiv.text(citiesNames); //show on html
+                tempsDiv.text("Temperature: " + temperatures); //show on html
+                humidDiv.text("Humidity: " + humid + "%"); //show on html
+                windyDiv.text("Wind Speeds: " + wind.speed + "MPH"); //show on html
+                todayContainer.append(weatherImg); //append to today container on html
+                todayContainer.append(namesDiv); //append to today container on html
+                todayContainer.append(tempsDiv); //append to today container on html
+                todayContainer.append(humidDiv); //append to today container on html
+                todayContainer.append(windyDiv); //append to today container on html
 
             });
     }
@@ -78,7 +76,6 @@ $(document).ready(function () {
         fetch(forecastBaseUrl2).then(function (rawResponse) {
             return rawResponse.json()
         }).then(function (data) {
-            console.log('Forecast', data);
             var coordinates = data.city.coord;
             getUVI(coordinates.lat, coordinates.lon);
             //array that collects data for 5 day forecast
@@ -114,11 +111,9 @@ $(document).ready(function () {
                     rows.append(windyDiv);
                     forecastContainer.append(rows);
                 }
-               
             }
-         
         });
-    
+
         function clearResults() {
             $('#forecast').html('');
             $('#today').html('');
@@ -127,42 +122,37 @@ $(document).ready(function () {
 
 
 
-    function getUVI(lat, lon) {
-    //lat=33.441792&lon=-94.037689&exclude=hourly,daily&appid={API key}
-        var UviURL = baseUviURL + 'lat=' + lat + '&lon=' + lon + '&exclude=hourly,daily&appid=' + apiKey;
-        fetch(UviURL).then(function(response) {
-            return response.json();
-        }).then(function(data) {
-            console.log('UV DATA', data)
-            var uviInfo = data.current.uvi;
-            todayContainer.append("UV Index: " + uviInfo);
-        });
-    }//store in local storage, get from local storage, display on html
-    function getHistorySearches() {
-        if (localStorage.getItem('historySearches')) {
-            historySearches = JSON.parse(localStorage.getItem('historySearches'));
-            for (var i = 0; i < historySearches.length; i++) {
-                var searchDiv = $('<button type = "button" class= "btn search-div">');
-                searchDiv.click(function(event) {
-                    event.preventDefault();
-                    var $this = $(event.target);
-                    var city = $this.text();
+        function getUVI(lat, lon) {
+            //lat=33.441792&lon=-94.037689&exclude=hourly,daily&appid={API key}
+            var UviURL = baseUviURL + 'lat=' + lat + '&lon=' + lon + '&exclude=hourly,daily&appid=' + apiKey;
+            fetch(UviURL).then(function (response) {
+                return response.json();
+            }).then(function (data) {
+                var uviInfo = data.current.uvi;
+                todayContainer.append("UV Index: " + uviInfo);
+            });
+        } //store in local storage, get from local storage, display on html
+        function getHistorySearches() {
+            if (localStorage.getItem('historySearches')) {
+                historySearches = JSON.parse(localStorage.getItem('historySearches'));
+                for (var i = 0; i < historySearches.length; i++) {
+                    var searchDiv = $('<button type = "button" class= "btn search-div">');
+                    searchDiv.click(function (event) {
+                        event.preventDefault();
+                        var $this = $(event.target);
+                        var city = $this.text();
+                        searchWeather(city);
+                        searchForecast(city);
 
-                    searchWeather(city);
-                    searchForecast(city);
-                   
-                });
-                searchDiv.text(historySearches[i]);
-                historyContainer.append(searchDiv);
-                
+                    });
+                    searchDiv.text(historySearches[i]);
+                    historyContainer.append(searchDiv);
+
+                }
             }
         }
+
+        getHistorySearches();
+        clearResults();
     }
-  
-    getHistorySearches();
-    clearResults();
-
-}
-
-   
 });
